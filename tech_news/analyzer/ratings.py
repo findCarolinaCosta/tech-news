07b548c210d988dcdcadfa3f666fe01c252f75f6
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-27 01:33:34 -0300

    Feat: top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
 # Requisito 10
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+
+    return [
+        (news["title"], news["url"])
+        for news in sorted(
+            find_news(), key=lambda x: x["comments_count"], reverse=True
+        )[:5]
+    ]
 
 
 # Requisito 11
