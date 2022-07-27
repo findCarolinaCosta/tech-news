cebcca8bd7693cddbf428af59afbf7070291c632
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-07-26 22:35:10 -0300

    Feat: scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,22 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(text=html_content)
+
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css("span.author > a ::text").get(),
+        "comments_count": len(selector.css(".comments-list li").getall()),
+        "summary": "".join(
+            selector.css(
+                "div.entry-content > p:nth-of-type(1) *::text"
+            ).getall()
+        ).strip(),
+        "tags": selector.css("section.post-tags ul li a::text").getall(),
+        "category": selector.css("span.label::text").get(),
+    }
 
 
 # Requisito 5
